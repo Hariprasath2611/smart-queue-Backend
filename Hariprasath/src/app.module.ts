a97c48cb2c8d0c9341d72406c732fc7282e97f93
Hariprasath2611 <hp5173690@gmail.com>
2026-02-06 03:16:45 +0530

    Updated project files

--- Hariprasath/src/app.module.ts
+++ Hariprasath/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bullmq';
+import { AuthModule } from './modules/auth/auth.module';
+import { QueueModule } from './modules/queue/queue.module';
+import { NotificationModule } from './modules/notifications/notifications.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { PrismaService } from './common/prisma.service';
+import { RedisService } from './common/redis.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    QueueModule,
+    NotificationModule,
+    AdminModule,
+  ],
+  providers: [PrismaService, RedisService],
 })
-export class AppModule {}
+export class AppModule { }
